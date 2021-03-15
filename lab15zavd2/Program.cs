fd2aea1b30749d66605f11ff097440e50c3aa948
Fanrite <76695059+Fanrite@users.noreply.github.com>
2021-03-16 01:38:03 +0200

    Добавьте файлы проекта.

---
+++ lab15zavd2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace lab15zavd2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string output = "";
+            string txt;
+
+            txt = Interaction.InputBox("...", "Введіть текст: ", ".капец.");
+            foreach (char num in txt)
+            {
+                if (num == '.') output += $"...";
+                else output += num;
+            }
+
+            MessageBox.Show($"Оброблений рядок: {output}", "Результат", MessageBoxButtons.OK);
+        }
+    }
+}
